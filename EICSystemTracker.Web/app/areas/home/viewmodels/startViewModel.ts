3c6c5432cf94e5afd6ac507ea8867486939ccc5d
Tim Paschedag <tpaschedag@gmail.com>
2016-01-22 00:43:22 -0600

    Added changes to better use system.js

--- EICSystemTracker.Web/app/areas/home/viewmodels/startViewModel.ts
+++ EICSystemTracker.Web/app/areas/home/viewmodels/startViewModel.ts
@@ -1,6 +1,10 @@
 ﻿import PageViewModel from '../../../framework/domain/PageViewModel';
+import ko from '../../../lib/knockout';
 
 class startViewModel extends PageViewModel {
+
+    public colorPalette: KnockoutObservable<string> = ko.observable("basic");
+    public selectedChoice: KnockoutObservable<string> = ko.observable("#ffffff");
 
     constructor() {
         super();
--- EICSystemTracker.Web/init.ts
+++ EICSystemTracker.Web/init.ts
@@ -4,21 +4,44 @@ System.config(<SystemConfig>{
         'jquery': 'scripts/jquery-2.1.4.min.js',
         'knockout': 'scripts/knockout-3.4.0.js',
         'pager': 'node_modules/pagerjs/pager.js',
-        'traceur': 'scripts/traceur.min.js'
+        'traceur': 'scripts/traceur.min.js',
+        'css': '/node_modules/systemjs/plugins/css.js',
+
+        // external plugin mappings.
+        'kendo': 'scripts/kendo/2016.1.112/kendo.core.min.js',
+        'kendoBindings': 'scripts/knockout/kendoBindings.js',
+        'bootstrapJs' : 'Scripts/bootstrap.js'
     },
     traceurOptions: {
         sourceMaps: false
-    }
+    },
+    buildCSS: false
 });
 
 Promise.all([
-    // CSS
-    System.import('content/bootstrap.css'),
-    System.import('content/kendo/2016.1.112/kendo.common.min.css'),
-    System.import('appStyles.css'),
 
-    // External Plugins.
-    System.import('Scripts/bootstrap.js'),
-    System.import('scripts/kendo/2016.1.112/kendo.core.min.js'),
-    System.import('app')
-]);
+    // Load frameworks first...
+    System.import('jquery'),
+    System.import('knockout')
+
+
+]).then(() => {
+
+    // Load all plugins...    
+    Promise.all([
+
+        // CSS
+        System.import('Content/bootstrap.css!css'),
+        System.import('Content/kendo/2016.1.112/kendo.common.min.css!css'),
+        System.import('appStyles.css!css'),
+
+        // External Plugins.
+        System.import('bootstrapJs'),
+        System.import('kendo'),
+        System.import('kendoBindings'),
+
+        // Load the main viewmodel page...
+        System.import('app.js')
+    ]);
+
+});
