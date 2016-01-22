c04eab73cd236888b5c0bd37948ef0356c38b43b
Tim Paschedag <tpaschedag@gmail.com>
2016-01-21 22:08:59 -0600

    Add files.

---
+++ EICSystemTracker.Contracts/domain/SystemTracking/EICSystemFaction.cs
@@ -0,0 +1,16 @@
+﻿using EICSystemTracker.Contracts.SystemTracking;
+
+namespace EICSystemTracker.Contracts.domain.SystemTracking
+{
+    public class EICSystemFaction : IEICSystemFaction
+    {
+        public int Id { get; }
+        public IEICSystem System { get; set; }
+        public IEICFaction Faction { get; set; }
+        public double Influence { get; set; }
+        public string CurrentState { get; set; }
+        public string PendingState { get; set; }
+        public string RecoveringState { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
