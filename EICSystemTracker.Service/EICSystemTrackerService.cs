c8fede23c508c9976e873cb4ce12c1fbe1333908
Tim Paschedag <tpaschedag@gmail.com>
2016-01-30 01:47:28 -0600

    add missing files... Add some things to ignore.

---
+++ EICSystemTracker.Service/EICSystemTrackerService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EICSystemTracker.Contracts;
+using EICSystemTracker.Contracts.Data;
+using EICSystemTracker.Contracts.SystemTracking;
+using EICSystemTracker.Data;
+
+namespace EICSystemTracker.Service
+{
+    public class EICSystemTrackerService : IEICSystemTrackerService
+    {
+        public List<IEICSystem> GetSystems()
+        {
+
+            // Get systems from 
+            throw new NotImplementedException();
+        }
+
+        public List<IEICSystemFaction> GetLatestSystemTrackingData()
+        {
+            using (var mySqlAdapter = EICDataFactory.GetDataAdapter(DataAdapterType.MySql))
+            {
+                return mySqlAdapter.GetLatestEICSystemFactionTracking();
+            }
+        }
+
+        public void UpdateSystemFactionInfo(IEICSystemFaction systemFaction)
+        {
+            using (var mySqlAdapter = EICDataFactory.GetDataAdapter(DataAdapterType.MySql))
+            {
+                mySqlAdapter.AddSystemFactionTracking(systemFaction);
+            }
+        }
+    }
+}
